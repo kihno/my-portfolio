9e7548f9cb0f68b2324defbc12f604687d5bb5cb
kihno <stistyle13@gmail.com>
2022-12-01 17:04:05 -0800

    Create responsive project grid for projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from './Navbar';
+import '../styles/Projects.css';
 
 const Projects = () => {
 
@@ -7,6 +8,71 @@ const Projects = () => {
         <main>
             <Navbar />
             <h1>Projects</h1>
+            <section className='projectContainer'>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='projectImage'></div>
+                    <div className='projectDescription'>
+                        <h3>Project Title</h3>
+                        <p>This is a description of the project. I will also explain what techniques I used to build this application.</p>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
