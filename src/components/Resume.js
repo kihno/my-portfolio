dbdff0d5f7049655d98909e2e5d88809c4e09f8d
kihno <stistyle13@gmail.com>
2022-12-20 12:47:35 -0800

    Insert resume pdf for view and download

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from './Navbar';
+import { Document, Page } from 'react-pdf';
+import myResume from '../pdf/Miles_Resume.pdf';
+import '../styles/Resume.css';
 
 const Resume = () => {
 
     return (
         <main>
             <Navbar />
-            <h1>Resume</h1>
+            <a href={myResume} target='_blank' rel='noopener noreferrer'>
+                <Document file={myResume} options={{workerSrc: "pdf.worker.js"}} className='resume'>
+                    <Page pageNumber={1} />
+                </Document>
+            </a>
         </main>
     )
 }
